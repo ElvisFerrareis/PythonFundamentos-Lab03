077445d2ad9af0de35ee8cfde3bec681c3399d8e
Elvis Jonas Ferrareis <elvis.ferrareis@gmail.com>
2022-09-27 14:45:02 -0300

    Codificação dos métodos da classe Hangman

--- forca_v1.py
+++ forca_v1.py
@@ -72,23 +72,44 @@ class Hangman:
 	# Método Construtor
 	def __init__(self, word):
 		self.posicao = 0
+		self.word = word
+		self.word_hidden = self.hide_word(word)
+		self.letras_certas = []
+		self.letras_erradas = []
 		
 	# Método para adivinhar a letra
 	def guess(self, letter):
-		
+		if letter in self.word:
+			self.letras_certas.append(letter)
+			self.unhide_word(letter)
+		else:
+			self.letras_erradas.append(letter)
+			self.posicao += 1
 		
 	# Método para verificar se o jogo terminou
 	def hangman_over(self):
-		
+		if self.posicao == len(board):
+			return True
+		else:
+			return False
 		
 	# Método para verificar se o jogador venceu
 	def hangman_won(self):
-		
+		if self.word_hidden.count('_') == 0:
+			return True
+		else:
+			return False
 
 	# Método para não mostrar a letra no board
 	def hide_word(self, word):
 		hidden = '_'
-		
+		return str([hidden for x in word])
+
+	# Método para mostrar a letra no board quando o jogador acertar a letra
+	def unhide_word(self, letter):
+		letter_positions = [x for x in range(0, len(self.word)) if letter == self.word[x]]
+		for x in letter_positions:
+			self.word_hidden[x] = self.word[x]
 		
 	# Método para checar o status do game e imprimir o board na tela
 	def print_game_status(self):
@@ -96,9 +117,9 @@ class Hangman:
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_word():
-        with open("palavras.txt", "rt") as f:
-                bank = f.readlines()
-        return bank[random.randint(0,len(bank))].strip()
+	with open("palavras.txt", "rt") as f:
+		bank = f.readlines()
+	return bank[random.randint(0,len(bank))].strip()
 
 
 # Função Main - Execução do Programa
